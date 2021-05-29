599262be885ea2c1777e7d178fc87c7cbe3a9285
huibizhang <naruto6404@hotmail.com>
2021-05-29 16:25:03 +0800

    update cli.js

--- scripts/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = function() {
-
-  console.log('\x1b[32m' + 'Vitailwind installed.\n\n' , '\x1b[37m');
-
-  console.log(`using \x1b[33m'npx vitailwind init'\x1b[37m to create configs.`);
-
-  return {}
-}

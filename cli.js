73424ce2ba076bffc07401b68683068c69a907d3
huibizhang <naruto6404@hotmail.com>
2021-05-29 17:06:12 +0800

    update cli.js

--- cli.js
+++ cli.js
@@ -1,3 +1,28 @@
 #!/usr/bin/env node
+var exec = require('child_process').exec;
 
-console.log('Tailwind TEST');
+module.exports = function() {
+
+  var cmd = exec(`npx tailwind init -p`, function(err, stdout, stderr) {
+    if (err) {
+      // handle error
+      console.log("From Plugin - " + err);
+    }
+
+    if(stderr){
+      console.log('\x1b[31m' + "Error:" + stderr,'\x1b[37m');
+    }
+
+    console.log(stdout);
+  });
+
+  cmd.on('exit', function (code) {
+      // return value from "npm build"
+      //console.log("code:" + code);
+      if(code==1){
+        //console.log('\x1b[31m' + `Tailwind watcher stopped.`,'\x1b[37m');
+      }
+  });
+
+  return {}
+}

1f3189d621151ad6e412e86e0cf709cf660bc20a
huibizhang <naruto6404@hotmail.com>
2021-05-29 17:40:11 +0800

    done cli.js

--- cli.js
+++ cli.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
-
 var exec = require('child_process').exec;
+var fs = require('fs')
 
-module.exports = function() {
-
-  console.log("starting vitailwind initialize...\n\n");
-
+function init() {
   var cmd = exec(`npx tailwind init -p`, function(err, stdout, stderr) {
     if (err) {
       // handle error
-      console.log("From Plugin - " + err);
+      if(!/already exists/g.test(err)){
+        console.log("From Plugin - " + err);
+      }
     }
 
     if(stderr){
@@ -17,15 +16,31 @@ module.exports = function() {
     }
 
     console.log(stdout);
+
+    if(!err && !stderr){
+      fs.writeFile(
+        './tailwind.css',
+        '@tailwind base;\n@tailwind components;\n@tailwind utilities;\n',
+        function (error) {
+          if(error){
+            console.log(error)
+          }else{
+            console.log('\x1b[33m' + "Create file:" + '\x1b[37m' + " tailwind.css\n")
+          }
+      })
+    }
   });
 
   cmd.on('exit', function (code) {
       // return value from "npm build"
       //console.log("code:" + code);
-      if(code==1){
-        //console.log('\x1b[31m' + `Tailwind watcher stopped.`,'\x1b[37m');
-      }
+      // if(code==1){
+      //   console.log('\x1b[31m' + `Tailwind watcher stopped.`,'\x1b[37m');
+      // }else{
+      //   console.log(`CODE: ${code}`)
+      // }
   });
-
-  return {}
 }
+
+console.log('\n\x1b[32m' + 'Starting vitailwind initialize...','\x1b[37m');
+init();

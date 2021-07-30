b93eec5453984692be7facb71f1bc25643b365e0
搋兔 <naruto6404@hotmail.com>
2021-07-30 08:25:02 +0800

    add: documents of create-vitawind

---
+++ docs/.vitepress/components/CreateVitawind.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="hp">
+    <img id="logo" :src="logoPath" />
+    <h1 style="font-size: 40px" class="bg-pink-700">create-vitawind</h1>
+    <span
+      style="
+        font-size: 20px;
+        color: #aaaaaa;
+        display: block;
+        margin: 5px 0 10px 0;
+      "
+      >Scaffolding for your tailwind project</span
+    >
+    <p align="center">
+      <span style="display: flex; gap: 5px">
+        <a href="https://npmjs.com/package/vite"
+          ><img
+            src="https://img.shields.io/npm/v/create-vitawind?style=flat-square"
+            alt="version"
+        /></a>
+        <a href="https://nodejs.org/en/about/releases/"
+          ><img
+            src="https://img.shields.io/node/v/create-vitawind?style=flat-square"
+            alt="node version"
+        /></a>
+        <!-- <br>
+    <a href="https://nodejs.org/en/about/releases/"><img src="https://img.shields.io/npm/dw/create-vitawind?style=flat-square" alt="npm-download"></a> -->
+      </span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateVitawind",
+  data() {
+    return {
+      logoPath: "/vitawind_pack.svg",
+      url: "/scaffolding/",
+    };
+  },
+};
+</script>
+
+<style>
+#hp {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  /* background-color:#DDDDDD; */
+}
+
+#logo {
+  width: 250px;
+  height: 250px;
+  object-fit: contain;
+}
+</style>

2396bb1f34d7aabf42672470337455258834a9b6
搋兔 <naruto6404@hotmail.com>
2021-08-04 05:40:28 +0800

    update: not ignore clone.cmd

---
+++ packages/create-vitawind/clone.cmd
@@ -0,0 +1,6 @@
+git clone git@github.com:huibizhang/template-cra-tailwind-jit-v2.2.git
+git clone git@github.com:huibizhang/template-cra-tailwind-jit-v2.1.4.git
+git clone git@github.com:huibizhang/template-ng-tailwind-jit.git
+git clone git@github.com:huibizhang/template-vuecli-tailwind-jit.git
+git clone git@github.com:huibizhang/template-vite-vue-tailwind-jit.git
+git clone git@github.com:huibizhang/template-vite-react-tailwind-jit.git

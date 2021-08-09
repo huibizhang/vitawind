9dafe093f9b05ffb96e2c7795bb93fbf52a7f6f2
搋兔 <naruto6404@hotmail.com>
2021-08-10 06:23:50 +0800

    add pnpm, fix tool selector

--- docs/.vitepress/components/Terminal.vue
+++ docs/.vitepress/components/Terminal.vue
@@ -23,36 +23,54 @@
           <div class="w-3 h-3 bg-green-600 hover:bg-green-400 rounded-full"></div>
         </div>
         <div class="inline-flex justify-end py-1 items-center" style="z-index:1;">
+          <!-- npm -->
           <label class="cursor-pointer">
             <input type="radio" :name="name" v-model="tool" :key="`${name}-npm`" class="appearance-none hidden peer" value="npm" />
             <div class="px-1.5 py-0.5 mx-2 rounded-md inline-block text-sm hover:bg-gray-500 peer-checked:bg-white/70 peer-checked:text-gray-700 transition-all">npm</div>
           </label>
           <div class="w-0.5 h-5/6 bg-gray-500/60" />
+          <!-- yarn -->
           <label class="cursor-pointer">
             <input type="radio" :name="name" v-model="tool" :key="`${name}-yarn`" class="appearance-none hidden peer" value="yarn" />
             <div class="px-1.5 py-0.5 mx-2 rounded-md inline-block text-sm hover:bg-gray-500 peer-checked:bg-white/70 peer-checked:text-gray-700 transition-all">yarn</div>
           </label>
           <div class="w-0.5 h-5/6 bg-gray-500/60" />
+          <!-- pnpm -->
           <label class="cursor-pointer">
             <input type="radio" :name="name" v-model="tool" :key="`${name}-pnpm`" class="appearance-none hidden peer" value="pnpm" />
             <div class="px-1.5 py-0.5 mx-2 rounded-md inline-block text-sm hover:bg-gray-500 peer-checked:bg-white/70 peer-checked:text-gray-700 transition-all">pnpm</div>
           </label>
         </div>
       </div>
-      <div class="w-full bg-gray-900 px-4 text-gray-300 text-sm overflow-x-auto">
-        <slot>
+      <div class="w-full bg-gray-900 px-4 text-gray-300 text-sm overflow-x-auto pr-4">
+        <slot class="inline-block">
         </slot>
       </div>
     </div>
     <div class="text-center">
-      <button class="bg-green-600 border-0 px-4 py-2 text-lg rounded-lg inset-0 mx-auto hover:scale-110 active:scale-90 transition-all text-white disabled:bg-gray-500 disabled:hover:scale-100" @click="$emit('copy')" :disabled="invalid">
-        Copy it !
+      <button 
+        :class="[
+          'min-w-0 min-h-0 px-4 py-2 inset-0 mx-auto transition-all',
+          'border-0 rounded-lg bg-green-600 hover:bg-green-500 active:bg-green-700 disabled:bg-yellow-300 disabled:ring-4 ring-yellow-400/50',
+          'text-lg disabled:text-base text-white disabled:text-yellow-700 disabled:text-left',
+          'hover:scale-110 active:scale-90 disabled:hover:scale-100'
+        ]"
+        @click="$emit('copy')" :disabled="invalid || !template"
+      >
+        {{ 
+          invalid || !template?
+          'Please setting project name and choose a template'
+          :
+          'Copy it !'
+         }}
       </button>
     </div>
   </div>
 </template>
 
 <script>
+import clsx from 'clsx'
+
 export default {
   name: "Terminal",
   props: ['name','tool','template'],
--- docs/scaffolding/index.md
+++ docs/scaffolding/index.md
@@ -10,8 +10,43 @@ head:
 sidebarDepth: 2
 ---
 
-<script setup>
+<script>
 import CreateVitawind from '../.vitepress/components/CreateVitawind.vue'
+import Badge from '../.vitepress/components/Badge.vue'
+import ToolToggle from '../.vitepress/components/ToolToggle.vue'
+
+export default{
+  data () {
+    return {
+      tool: 'npm',
+      storage: undefined
+    }
+  },
+  mounted () {
+    let tool = ''
+    if (typeof window !== 'undefined') {
+      if(window.localStorage.length>0) {
+        tool = window.localStorage.getItem('tool')
+      }
+    }
+    this.tool = tool?tool:'npm';
+    this.ct(this.tool)
+  },
+  methods:{
+    ct (event) {
+      this.tool = event
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem('tool',event)
+      }
+      // if (this.storage = !) {
+      //   this.storage.setItem('tool',event)
+      // }
+    }
+  },
+  components: {
+    CreateVitawind,Badge,ToolToggle
+  }
+}
 </script>
 
 <CreateVitawind />
@@ -24,14 +59,31 @@ Nothing even "setup tailwindcss in jit mode" can stop you developing! Now, you c
 
 To start, open terminal at your directory where you want to put project. And, run following commands:
 
+<ToolToggle :name="'tool-vue-example-install'" :tool="tool" @tool="ct($event)"><div v-if="tool === 'npm' || tool === 'pnpm'">
+
 ```bash
-npm init vitawind@latest {project-name} -- {template}
+npm init vitawind@latest {project-name} -- --vue
 ```
+</div><div v-if="tool === 'yarn'">
+
+```bash
+yarn create vitawind {project-name} --vue
+```
+</div></ToolToggle>
+
 base on this command, please give project name and choose a template, for example:
+
+<ToolToggle :name="'tool-vue-install'" :tool="tool" @tool="ct($event)"><div v-if="tool === 'npm' || tool === 'pnpm'">
 
 ```bash
 npm init vitawind@latest my-first-app -- --vue
 ```
+</div><div v-if="tool === 'yarn'">
+
+```bash
+yarn create vitawind my-first-app --vue
+```
+</div></ToolToggle>
 
 ### Template List
 Go to [See all templates](./templates). Or, you can using our [Creator](./creator) tool to quick start.

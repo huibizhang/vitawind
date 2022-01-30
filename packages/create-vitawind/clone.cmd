29047a27a1cd92f43fc0e43288c618b59dda53f9
huibizhang <naruto6404@hotmail.com>
2022-01-30 18:59:24 +0000

    add: commands for remove `.git`

--- packages/create-vitawind/clone.cmd
+++ packages/create-vitawind/clone.cmd
@@ -7,3 +7,13 @@ git clone git@github.com:huibizhang/template-vite-react-ts-tailwind-v3.git
 @REM git clone git@github.com:huibizhang/template-vuecli5-tailwind-jit.git
 @REM git clone git@github.com:huibizhang/template-cra-tailwind-jit.git
 @REM git clone git@github.com:huibizhang/template-ng-tailwind-jit.git
+
+@REM remove .git
+cd template-vite-vue-tailwind-v3
+rm -rf .git
+cd ../template-vite-vue-ts-tailwind-v3
+rm -rf .git
+cd ../template-vite-react-tailwind-v3
+rm -rf .git
+cd ../template-vite-react-ts-tailwind-v3
+rm -rf .git

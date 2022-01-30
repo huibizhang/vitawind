f811bb87680f3cd371cebd563cbb2df4d162a469
huibizhang <naruto6404@hotmail.com>
2022-01-30 19:15:53 +0000

    update: remove subrepos

--- packages/create-vitawind/clone.cmd
+++ packages/create-vitawind/clone.cmd
@@ -7,3 +7,25 @@ git clone git@github.com:huibizhang/template-vuecli-tailwind-jit.git
 git clone git@github.com:huibizhang/template-vuecli5-tailwind-jit.git
 git clone git@github.com:huibizhang/template-cra-tailwind-jit.git
 git clone git@github.com:huibizhang/template-ng-tailwind-jit.git
+
+@REM remove .git
+cd node_modules
+cd ../template-vite-vanilla-tailwind-jit
+rm -rf .git
+cd ../template-vite-vue-tailwind-jit
+rm -rf .git
+cd ../template-vite-vue-ts-tailwind-jit
+rm -rf .git
+cd ../template-vite-react-tailwind-jit
+rm -rf .git
+cd ../template-vite-react-ts-tailwind-jit
+rm -rf .git
+cd ../template-vuecli-tailwind-jit
+rm -rf .git
+cd ../template-vuecli5-tailwind-jit
+rm -rf .git
+cd ../template-cra-tailwind-jit
+rm -rf .git
+cd ../template-ng-tailwind-jit
+rm -rf .git
+cd ..

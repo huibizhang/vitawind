8bd87c192c39d7d1a2ae81a68c655375836f1d8b
huibizhang <naruto6404@hotmail.com>
2022-02-01 19:39:35 +0000

    add: template `cra` and `ng` to list

--- packages/create-vitawind/sync-repos.js
+++ packages/create-vitawind/sync-repos.js
@@ -1,4 +1,5 @@
 const { execSync } = require('child_process');
+const fs = require('fs');
 
 const from = "huibizhang"
 
@@ -7,9 +8,15 @@ const repos = [
   "template-vite-vue-ts-tailwind-v3",
   "template-vite-react-tailwind-v3",
   "template-vite-react-ts-tailwind-v3",
+  "template-cra-tailwind-v3",
+  "template-ng-tailwind-v3",
 ]
 
 repos.forEach(repo => {
+  if(fs.existsSync(repo)){
+    fs.rmdirSync(repo, { recursive: true, force: true })
+    console.log(`removed repo '${repo}'`)
+  }
   console.log(`sync repo '${repo}'`)
   execSync(`git clone git@github.com:${from}/${repo}.git\ncd ${repo}\nrm -rf .git`)
 })

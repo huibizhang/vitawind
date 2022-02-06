ceca0c90c6aa7278c9207ace364837e1b57542c7
huibizhang <naruto6404@hotmail.com>
2022-02-06 21:12:11 +0000

    fix: gitignore

--- docs-new/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from '/workspaces/vitawind/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
--- docs-new/.vuepress/.temp/pages/zh/index.html.vue
+++
@@ -1,2 +0,0 @@
-<template><h1 id="嗨" tabindex="-1"><a class="header-anchor" href="#嗨" aria-hidden="true">#</a> 嗨!</h1>
-</template>

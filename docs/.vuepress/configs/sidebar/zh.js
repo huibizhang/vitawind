accaee266dab26e5ed95b1a036201f59f3407047
搋兔 Tryto <35431381+huibizhang@users.noreply.github.com>
2022-02-08 07:24:10 +0800

    fix: "/zh/" instead "zh/"

--- docs/.vuepress/configs/sidebar/zh.js
+++ docs/.vuepress/configs/sidebar/zh.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   "/zh/guide-for-vitawind/": [
     {
       text: "Vitawind 指南",
@@ -8,20 +7,22 @@ module.exports = {
         {
           text: "使用方法",
           link: "/zh/guide-for-vitawind/usage/",
-=======
-  "zh/guide-for-vitawind/": [
+        },
+      ],
+    },
+  ],
+  "/zh/guide-for-vitawind/": [
     {
       text: "Vitawind 指南",
       children: [
-        "zh/guide-for-vitawind/",
+        "/zh/guide-for-vitawind/",
         {
           text: "配置",
-          link: "zh/guide-for-vitawind/configurations/",
+          link: "/zh/guide-for-vitawind/configurations/",
         },
         {
           text: "使用方法",
-          link: "zh/guide-for-vitawind/usage/",
->>>>>>> 66655a1 (updated: v2 blog using vuepress)
+          link: "/zh/guide-for-vitawind/usage/",
         },
         {
           text: "配置",
@@ -30,49 +31,28 @@ module.exports = {
       ],
     },
   ],
-<<<<<<< HEAD
   "/zh/scaffolding/": [
-=======
-  "zh/scaffolding/": [
->>>>>>> 66655a1 (updated: v2 blog using vuepress)
     {
       text: "起手式",
       children: [
         {
           text: "簡介",
-<<<<<<< HEAD
           link: "/zh/scaffolding/",
         },
         {
           text: "範本 (Templates)",
           link: "/zh/scaffolding/templates/",
-=======
-          link: "zh/scaffolding/",
-        },
-        {
-          text: "模板專案",
-          link: "zh/scaffolding/templates/",
->>>>>>> 66655a1 (updated: v2 blog using vuepress)
         },
       ],
     },
     {
-<<<<<<< HEAD
       text: "工具",
       children: [
         {
           text: "Creator",
           link: "/zh/scaffolding/creator/",
         },
-<<<<<<< HEAD:docs/.vuepress/configs/sidebar/zh.js
       ],
-=======
-      ]
-=======
-      text: "Creator",
-      link: "zh/scaffolding/creator/",
->>>>>>> 66655a1 (updated: v2 blog using vuepress)
->>>>>>> 2c996b7 (updated: v2 blog using vuepress):docs-new/.vuepress/configs/sidebar/zh.js
     },
   ],
 };

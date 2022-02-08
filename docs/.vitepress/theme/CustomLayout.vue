672fe159683b6c75e204f9cdfeb96738a8bfc547
搋兔 Tryto <35431381+huibizhang@users.noreply.github.com>
2022-02-08 14:22:56 +0800

    Create CustomLayout.vue

---
+++ docs/.vitepress/theme/CustomLayout.vue
@@ -0,0 +1,13 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <div class="flex flex-col w-screen h-screen">
+    <div class="flex-none flex p-2 justifycenter bg-green-500 text-white">
+      You're looking v1 documents.
+    </div>
+    <Layout class="flex-1" />
+  </div>
+</template>

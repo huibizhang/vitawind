5d1981d2548764036b413a67230a8b7dbd2c3984
搋兔 Tryto <35431381+huibizhang@users.noreply.github.com>
2022-02-08 14:43:16 +0800

    Update CustomLayout.vue

--- docs/.vitepress/theme/CustomLayout.vue
+++ docs/.vitepress/theme/CustomLayout.vue
@@ -5,7 +5,7 @@ const { Layout } = DefaultTheme
 
 <template>
   <div class="flex flex-col w-screen h-screen">
-    <div class="flex-none flex p-2 justifycenter bg-green-500 text-white">
+    <div class="flex-none flex p-2 h-10 justify-center bg-green-500 text-white">
       You're looking v1 documents.
     </div>
     <Layout class="flex-1">

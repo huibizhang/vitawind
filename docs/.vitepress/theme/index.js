96f70c820b566484b068873d9bb94a771a133eb0
搋兔 Tryto <35431381+huibizhang@users.noreply.github.com>
2022-02-08 14:17:57 +0800

    Update index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,8 +1,10 @@
 import DefaultTheme from 'vitepress/theme'
+import CustomLayout from './CustomLayout.vue'
 import './index.css'
 
 export default {
   ...DefaultTheme,
+  Layout: CustomLayout,
   enhanceApp({ app }) {
     app.component('MyButton')
   }

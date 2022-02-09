0825c5612ba465258492ac0ed0cdc1de02e03b94
搋兔 <naruto6404@hotmail.com>
2022-02-10 03:27:52 +0800

    add: banner

--- docs/.vitepress/theme/CustomLayout.vue
+++ docs/.vitepress/theme/CustomLayout.vue
@@ -1,17 +1,31 @@
 <script setup>
-import DefaultTheme from 'vitepress/theme'
-const { Layout } = DefaultTheme
+import DefaultTheme from "vitepress/theme";
+const { Layout } = DefaultTheme;
 </script>
 
 <template>
-  <div class="flex flex-col w-screen h-screen">
-    <div class="flex-none flex p-2 h-10 justify-center bg-green-500 text-white">
-      You're looking v1 documents.
+  <div class="flex flex-col w-screen h-screen overflow-x-hidden">
+    <div
+      class="flex-none flex h-8 justify-center bg-gradient-to-r from-green-500 to-blue-400 text-white items-center fixed w-screen z-50"
+    >
+      You're looking v1 documents. For newest content, see&nbsp;
+      <a href="https://vitawind.vercel.app" class="text-white underline"
+        >v2.x document</a
+      >.
     </div>
     <Layout class="flex-1">
-      <template #page-bottom>
-        TEST AREA
-      </template>
+      <template #page-bottom> TEST AREA </template>
     </Layout>
   </div>
 </template>
+
+<style>
+.nav-bar {
+  top: 2rem !important;
+}
+
+.sidebar,
+.theme {
+  margin-top: 2rem;
+}
+</style>
